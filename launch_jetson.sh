35eac6ad8deea925ff86e36fc6f8a747a5178fc6
Haoru <hxue@ucsd.edu>
2021-05-27 12:09:59 -0700

    added u-blox driver

--- launch_jetson.sh
+++ launch_jetson.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 
-sudo docker run --name purduesurvey -it --rm \
+sudo docker run --name purduesurvey-jetson -it --rm \
   --privileged \
   --network=host \
-  --mount type=volume,source=purdue-survey,dst=/root/ros_ws \
+  --mount type=volume,source=purdue-survey-jetson,dst=/root/ros_ws \
   --env="DISPLAY=$DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --env="XAUTHORITY=$XAUTH" \
   --volume="$XAUTH:$XAUTH" \
-  haoru233/purdue-gp-track-survey:ubuntu
+  haoru233/purdue-gp-track-survey:jetson 
